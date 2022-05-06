2dc430bc70a23ad1eec1296709c436e64ab68677
luizottavio <102751708+luizottavio@users.noreply.github.com>
2022-05-06 17:49:38 -0300

    Update ex008.js

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,6 @@
-console.log('Esta fauncionando!')
+var vel = 88
+console.log (`A veloidade do sou carro é de ${vel}km/h`)
+if( vel > 80 ) {
+    console.log (`Você passou o limite de velocidade MULTADO `)
+}
+console.log (`Dirija sempre com muita atenção!`)
