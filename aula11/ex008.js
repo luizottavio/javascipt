0febdf2a6b496b94791cc726a008d60542d0df3f
luizottavio <102751708+luizottavio@users.noreply.github.com>
2022-05-16 15:54:17 -0300

    Update ex008.js

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,6 +1,6 @@
 var vel = 88
 console.log (`A veloidade do sou carro é de ${vel}km/h`)
 if( vel > 80 ) {
-    console.log (`Você passou o limite de velocidade MULTADO `)
+    console.log (`Você passou o limite de velocidade. MULTADO!! `)
 }
 console.log (`Dirija sempre com muita atenção!`)
