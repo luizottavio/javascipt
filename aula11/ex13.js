d15ac6b5b4e858f8f407498a481f3808a6467037
luizottavio <102751708+luizottavio@users.noreply.github.com>
2022-06-22 15:55:18 -0300

    Create ex13.js

---
+++ aula11/ex13.js
@@ -0,0 +1,30 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+switch(diaSem) {
+    case 0 :
+        console.log('Domingo')
+        break
+    case 1 : 
+        console.log('Segunda')
+        break
+    case 2 : 
+        console.log('terça')
+        break
+    case 3 : 
+        console.log('quarta')
+        break
+    case 4 : 
+        console.log('quinta')
+        break
+    case 5 : 
+        console.log('sexta')
+        break
+    case 6 : 
+       console.log('sabado')
+       break
+    default:
+       console.log('[ERRO] Dia invalido')
+       break
+}
+
