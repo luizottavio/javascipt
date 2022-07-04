8c45388d3c10a3dc3a1dcfe0d2bff3cc943c19e7
luizottavio <102751708+luizottavio@users.noreply.github.com>
2022-07-04 16:51:20 -0300

    Update script.js

--- aula12/ex14/script.js
+++ aula12/ex14/script.js
@@ -2,17 +2,20 @@ function carregar() {
     var hos = window.document.getElementById('hos')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+   var hora = data.getHours()
     hos.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12 ) {
         //Bom Dia!
+        document.body.style.background = '#026873'
         img.src = 'manha.400px.png'
     } else if (hora >= 12 && hora <= 18) {
         //Boa Tarde! 
+        document.body.style.background = '#595736'
         img.src = 'tarde.400px.png'
     } else { 
         //Boa Noite!
         img.src = 'noite.400px.png'
+        document.body.style.background = '#295073'
     }
 
 }
